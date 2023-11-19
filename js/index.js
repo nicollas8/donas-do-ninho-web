b9eff5f591a86bba6e7dbb3861aea6670d4a4d52
Nicollas Santos <nicollas.msantos12@gmail.com>
2023-11-18 21:30:51 -0300

    0.1
    - Alteração no link de dowload

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const email = document.getElementById("email")
 const dowload = document.getElementById("dowload")
 
 dowload.addEventListener('click', () => {
-  location.href = "https://drive.google.com/file/d/10sSFKXIwm0Rpb5ztYWWZ7ay7G7exc7wq/view?usp=drive_link"
+  location.href = "https://drive.google.com/file/d/10sSFKXIwm0Rpb5ztYWWZ7ay7G7exc7wq/view?usp=sharing"
 })
 
 insta.addEventListener('click', () =>{
